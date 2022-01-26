d976bb487cdf2533f5c0eebf086c8ff1385720c9
ketan patel <ketanjp79@yahoo.com>
2022-01-26 16:34:23 +0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ketanjp79/hellogo
+
+go 1.17
+
+require(
+	github.com/ketanjp79/mystrings v0.0.0	
+)
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import (
+	"fmt"
+	"github.com/ketanjp79/mystrings"
+)
+func main(){
+	fmt.Println(mtstrings.Reverse("hello world"))
+}
